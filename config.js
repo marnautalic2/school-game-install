35a357b6c127ae41d031bb6965e0bc801b7e5fb0
marnautalic2 <Mislav.Arnautalic@teladoc.com>
2026-02-09 10:19:58 +0100

    turn off demo mode

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.APP_CONFIG = {
   appName: "Sprout Tap",
   orgName: "Your Org",
   buildLabel: "prototype-0.1",
-  demoMode: true,
+  demoMode: false,
   landingUrl: "https://marnautalic2.github.io/school-game-install/",
   iosTestflightUrl: "https://testflight.apple.com/join/XXXXXXX",
   androidApkUrl: "https://github.com/marnautalic2/school-game-install/releases/download/v.0.1.0-demo/schoolgame.apk",
