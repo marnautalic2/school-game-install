e5aae1b9a6cc61899565fdd84014b8092f20d897
marnautalic2 <Mislav.Arnautalic@teladoc.com>
2026-02-09 10:13:19 +0100

    update config for demo

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ window.APP_CONFIG = {
   demoMode: true,
   landingUrl: "https://marnautalic2.github.io/school-game-install/",
   iosTestflightUrl: "https://testflight.apple.com/join/XXXXXXX",
-  androidApkUrl: "https://github.com/ORG/REPO/releases/latest/download/YourApp.apk",
+  androidApkUrl: "https://github.com/marnautalic2/school-game-install/releases/download/v.0.1.0-demo/schoolgame.apk",
   qrImagePath: "assets/qr.png",
   demoUrl: "demo.html",
   supportEmail: "support@example.com"
